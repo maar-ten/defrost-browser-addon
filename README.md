d8a4411e74df0b52688b213b16aa1595b9c79f48
maar-ten <maar-ten@users.noreply.github.com>
2020-04-25 16:12:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+![Defrost](/icon.svg)
+
+Defrost
+===
+Some websites freeze the user's experience with popups that you need to close before you can continue browsing. Some website's popups cannot be closed and  block you from going any further. This browser add-on defrosts those websites and let you continue on your way. 
+
+Available from
+---
+**[Firefox Add-ons](https://addons.mozilla.org/en-US/firefox/addon/defrost/)**
+
+Introduction
+---
+After installing this browser add-on there'll appear an icon in the address bar when visiting on of the supported websites. Clicking that icon will remove the popup so you can continue browsing the website.
+
+![Screenprint](instagram_example.png)
+
+Permissions
+---
+This browser extension doesn't require any permissions
+
---
+++ background.js
@@ -0,0 +1,28 @@
+browser.pageAction.onClicked.addListener(tab => {
+    const hostname = new URL(tab.url).hostname.toLocaleLowerCase();
+    if (hostname.includes('facebook')) {
+        unfreezeFacebook();
+    } else if (hostname.includes('instagram')) {
+        unfreezeInstagram();
+    }
+});
+
+function unfreezeFacebook() {
+    const NODE_SELECTOR = "#pagelet_growth_expanding_cta";
+    const REMOVE_NODE = `document.querySelector("${NODE_SELECTOR}").remove();`;
+
+    browser.tabs.executeScript({
+        code: REMOVE_NODE
+    });
+}
+
+function unfreezeInstagram() {
+    const CSS = "body { overflow-y: scroll !important }";
+    const NODE_SELECTOR = "div[role='presentation']";
+    const REMOVE_NODE = `document.querySelector("${NODE_SELECTOR}").remove();`;
+
+    browser.tabs.insertCSS({ code: CSS });
+    browser.tabs.executeScript({
+        code: REMOVE_NODE
+    });
+}
